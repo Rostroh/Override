121f92388384d5bf79f0920daad7711f3cc1124e
rostroh <rostroh@made-f0Br7s8.clusters.42paris.fr>
2023-10-21 13:44:37 +0200

    source level5

--- level05/source.c
+++ level05/source.c
@@ -1,1 +1,14 @@
-int			
+#include <stdio.h>
+
+void		main(void)
+{
+	char	buf[0x64];
+
+	fgets(buf, 0x64, stdin);
+	for (int i = 0; i < strlen(buf); i++)
+	{
+		if (buf[i] > '@' && buf[i] <= 'Z')
+			buf[i] += 0x20;
+	}
+	printf(buf);
+}
